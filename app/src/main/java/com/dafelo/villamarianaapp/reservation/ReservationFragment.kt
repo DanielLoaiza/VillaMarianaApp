ca749cf2b6a47415005a5b45d31c05e36a9adf2d
Daniel <daniel.loaiza@rappi.com>
2020-01-01 13:22:05 -0500

    Date picker dialog

--- app/src/main/java/com/dafelo/villamarianaapp/reservation/ReservationFragment.kt
+++ app/src/main/java/com/dafelo/villamarianaapp/reservation/ReservationFragment.kt
@@ -1,5 +1,6 @@
 package com.dafelo.villamarianaapp.reservation
 
+import android.app.DatePickerDialog
 import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
@@ -13,8 +14,11 @@ import com.dafelo.villamarianaapp.MainActivity
 
 import com.dafelo.villamarianaapp.R
 import com.dafelo.villamarianaapp.databinding.ReservationFragmentBinding
-import com.dafelo.villamarianaapp.rooms.RoomsViewModel
+import java.math.BigDecimal
+import java.util.*
 import javax.inject.Inject
+
+
 
 class ReservationFragment : Fragment() {
 
@@ -29,11 +33,37 @@ class ReservationFragment : Fragment() {
 
     private lateinit var binding: ReservationFragmentBinding
 
+    private val c = Calendar.getInstance()
+    private val year = c.get(Calendar.YEAR)
+    private val month = c.get(Calendar.MONTH)
+    private val day = c.get(Calendar.DAY_OF_MONTH)
+    private val ZERO = "0"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.reservation_fragment, container, false)
+        binding.textViewStartDate.setOnClickListener {
+            val datePicker = DatePickerDialog(
+                this@ReservationFragment.requireContext(),
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                    val currentMonth = month + 1
+
+                    val formattedDay = if (dayOfMonth < 10) ZERO + dayOfMonth.toString() else dayOfMonth.toString()
+
+                    //Formateo el mes obtenido: antepone el 0 si son menores de 10
+                    val formattedMonth =
+                        if (currentMonth < 10) ZERO + currentMonth.toString() else currentMonth.toString()
+                    //Muestro la fecha con el formato deseado
+                    binding.textViewStartDate.text = "$formattedDay/$formattedMonth/$year"
+                }, year, month, day
+            )
+
+            datePicker.datePicker.minDate = System.currentTimeMillis() - 1000
+
+            datePicker.show()
+        }
         return binding.root
     }
 
