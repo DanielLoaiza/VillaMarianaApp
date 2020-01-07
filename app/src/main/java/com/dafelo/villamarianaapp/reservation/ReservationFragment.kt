604f10e1ca6e965490b27788594e926a2a45fbca
Daniel <daniel.loaiza@rappi.com>
2020-01-07 12:34:35 -0500

    implemented databinding on reservations, shared click listener for date picker

--- app/src/main/java/com/dafelo/villamarianaapp/reservation/ReservationFragment.kt
+++ app/src/main/java/com/dafelo/villamarianaapp/reservation/ReservationFragment.kt
@@ -8,16 +8,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.dafelo.villamarianaapp.MainActivity
 
 import com.dafelo.villamarianaapp.R
 import com.dafelo.villamarianaapp.databinding.ReservationFragmentBinding
-import java.math.BigDecimal
 import java.util.*
 import javax.inject.Inject
-
 
 
 class ReservationFragment : Fragment() {
@@ -44,27 +43,37 @@ class ReservationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.reservation_fragment, container, false)
-        binding.textViewStartDate.setOnClickListener {
+
+        val listener = getDateClickListener()
+        binding.textViewStartDate.setOnClickListener(listener)
+        binding.textViewEndDate.setOnClickListener(listener)
+        binding.viewmodel = viewModel
+        return binding.root
+    }
+
+    private fun getDateClickListener(): View.OnClickListener {
+
+        return View.OnClickListener {
             val datePicker = DatePickerDialog(
-                this@ReservationFragment.requireContext(),
+                requireContext(),
                 DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                     val currentMonth = month + 1
 
-                    val formattedDay = if (dayOfMonth < 10) ZERO + dayOfMonth.toString() else dayOfMonth.toString()
+                    val formattedDay =
+                        if (dayOfMonth < 10) ZERO + dayOfMonth.toString() else dayOfMonth.toString()
 
-                    //Formateo el mes obtenido: antepone el 0 si son menores de 10
                     val formattedMonth =
                         if (currentMonth < 10) ZERO + currentMonth.toString() else currentMonth.toString()
-                    //Muestro la fecha con el formato deseado
-                    binding.textViewStartDate.text = "$formattedDay/$formattedMonth/$year"
+
+                    it as TextView
+                    it.text = "$formattedDay/$formattedMonth/$year"
                 }, year, month, day
             )
 
             datePicker.datePicker.minDate = System.currentTimeMillis() - 1000
-
             datePicker.show()
+
         }
-        return binding.root
     }
 
     override fun onAttach(context: Context) {
--- app/src/main/java/com/dafelo/villamarianaapp/reservation/ReservationViewModel.kt
+++ app/src/main/java/com/dafelo/villamarianaapp/reservation/ReservationViewModel.kt
@@ -1,10 +1,21 @@
 package com.dafelo.villamarianaapp.reservation
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.dafelo.villamarianaapp.components.qualifiers.FragmentScope
 import javax.inject.Inject
 
 @FragmentScope
 class ReservationViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val startDate = ObservableField("")
+    val endDate = ObservableField("")
+    val name = ObservableField("")
+    val id = ObservableField("")
+    val address = ObservableField("")
+    val insurance = ObservableField("")
+    val email = ObservableField("")
+    val phone = ObservableField("")
+
+    fun saveReservation() {}
 }
